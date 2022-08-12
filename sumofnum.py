020d032003cdd05a3ed78ba30f554e16704fd7fb
Sanjay Kumar Bais <sanjaykumarvaishya64@gmail.com>
2022-08-12 21:47:19 +0530

    all messages

--- sumofnum.py
+++ sumofnum.py
@@ -1,6 +1,6 @@
-n = int(input("please enter an integer: "))
+n = int(input("Please enter an integer: "))
 a = 0
 for i in range(1, n+1):
     if i<=n:
         a = a + i
-print("the sum of entered integer is: " + str(a))
+print("The sum of entered integer is: " + str(a))
